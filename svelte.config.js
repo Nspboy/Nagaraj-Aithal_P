5a27e3b414f0faaa690c3d6861f3f3a6f600d5f6
Nspboy <nagarajanagaraj.2003@gmail.com>
2026-06-21 10:20:05 +0530

    github error correction

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,7 +11,13 @@ const config = {
 		})
 	],
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			fallback: 'index.html',
+			pages: 'build',
+			assets: 'build',
+			precompress: false,
+			strict: false
+		}),
 		prerender: {
 			default: true,
 		},
